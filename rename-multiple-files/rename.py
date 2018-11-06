a2648712389e68ac882e7e074634e39b5ca37c6b
Sh-SK <44041955+Sh-SK@users.noreply.github.com>
2018-11-06 23:36:44 +0300

    v0.2

--- rename-multiple-files/rename.py
+++ rename-multiple-files/rename.py
@@ -1,14 +1,17 @@
 import os
-print("Добро пожаловать в программу, которая изменяет название файла в соответствии с заданным шаблоном!")
-# path = input("Введите путь к директории с файлами: ")  # E:\ZennoPoster\Projects\xakep_archieve\test  Enter path to folder:
-path = r"E:\ZennoPoster\Projects\xakep_archieve\test"
-str1 = input("Что заменить в названии файла: ")  # xa-
-str2 = input("Чем заменить: ")  # xakep #
+print("Добро пожаловать в программу, которая изменяет название файлов в соответствии с заданным шаблоном!")
+path = input("Введите путь к директории с файлами: ")  # E:\Dev\ZennoPoster\Projects\xakep_archieve\test
+# paths = paths.split("\\")
+# path = os.path.join(paths)
 
-# os.rename(src, dst, *, src_dir_fd=None, dst_dir_fd=None) - переименовывает файл или директорию из src в dst.
+str1 = input("Что заменить в названии файла: ")
+str2 = input("Чем заменить: ")
 
 for filename in os.listdir(path):
     os.chdir(path)
     name = filename
     name = name.replace(str1, str2)
-    os.rename(filename, name)
+    try:
+        os.rename(filename, name)
+    except(OSError):
+        print("Ошибка в имени файла.")
