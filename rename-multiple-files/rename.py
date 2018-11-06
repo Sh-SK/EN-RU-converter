9539f906cde09facc80b8b1ebb96fdc01f04807b
Sh-SK <44041955+Sh-SK@users.noreply.github.com>
2018-11-06 23:51:10 +0300

    try-except

--- rename-multiple-files/rename.py
+++ rename-multiple-files/rename.py
@@ -1,17 +1,20 @@
 import os
+
 print("Добро пожаловать в программу, которая изменяет название файлов в соответствии с заданным шаблоном!")
-path = input("Введите путь к директории с файлами: ")  # E:\Dev\ZennoPoster\Projects\xakep_archieve\test
-# paths = paths.split("\\")
-# path = os.path.join(paths)
-
-str1 = input("Что заменить в названии файла: ")
-str2 = input("Чем заменить: ")
-
-for filename in os.listdir(path):
+path = input("Введите путь к директории с файлами: ")
+try:
     os.chdir(path)
-    name = filename
-    name = name.replace(str1, str2)
-    try:
+    str1 = input("Что заменить в названии файла: ")
+    str2 = input("Чем заменить: ")
+
+    for filename in os.listdir(path):
+        name = filename
+        name = name.replace(str1, str2)
         os.rename(filename, name)
-    except(OSError):
-        print("Ошибка в имени файла.")
+except Exception:
+    print(err)
+
+
+
+
+
