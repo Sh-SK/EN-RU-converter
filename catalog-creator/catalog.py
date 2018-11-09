4871dbef94c19a82792e86945d7e7a256c60d716
Sh-SK <44041955+Sh-SK@users.noreply.github.com>
2018-11-09 20:04:44 +0300

    v0.2

--- catalog-creator/catalog.py
+++ catalog-creator/catalog.py
@@ -8,9 +8,19 @@ try:
     for i, filename in enumerate(os.listdir(path)):
         name = filename
         name = name.rstrip('.pdf')
+        name = name.split(' - ')
+        author = name[0]
+        author = author.strip()
+        title = name[1].split('(')[0].strip()
+        year = name[1].split('(')[1].rstrip(')')
         with open("catalog.txt", "a") as file:
-            file.write(name + '\n')
+            file.write(author + '|' + title + '|' + year + '\n')
     # file.close()
     print("Обработано {} файлов".format(i + 1))
 except Exception as err:
     print(err)
+
+# отделение авторов от названия и названия от года
+# создание класса Книга
+# создание CSV-файла
+# косяк когда в названии есть -
