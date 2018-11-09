f6bfb80b08de2122d8c90d5dbbb66cb3070043d1
Sh-SK <44041955+Sh-SK@users.noreply.github.com>
2018-11-09 21:09:10 +0300

    v0.3

--- catalog-creator/catalog.py
+++ catalog-creator/catalog.py
@@ -1,26 +1,24 @@
-import os
+import os, csv
 
-print("Добро пожаловать в программу Catalog creator!\nОна создает каталог из названий файлов в папке.")
+print("Добро пожаловать в программу Catalog creator!\nОна создает каталог из названий файлов в папке.\n")
 path = input("Введите путь к директории с файлами: ")
 try:
     os.chdir(path)
-    # file = open("catalog.txt", "w")
     for i, filename in enumerate(os.listdir(path)):
+        book = []
         name = filename
         name = name.rstrip('.pdf')
         name = name.split(' - ')
         author = name[0]
         author = author.strip()
+        book.append(author)
         title = name[1].split('(')[0].strip()
+        book.append(title)
         year = name[1].split('(')[1].rstrip(')')
-        with open("catalog.txt", "a") as file:
-            file.write(author + '|' + title + '|' + year + '\n')
-    # file.close()
+        book.append(year)
+        with open("catalog.csv", "a") as file:
+            w = csv.writer(file, delimiter='|')
+            w.writerow(book)
     print("Обработано {} файлов".format(i + 1))
 except Exception as err:
     print(err)
-
-# отделение авторов от названия и названия от года
-# создание класса Книга
-# создание CSV-файла
-# косяк когда в названии есть -
